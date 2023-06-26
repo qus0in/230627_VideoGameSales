bd9c5e793cb7eae907f09364aa888d9d82357a4a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-06-27 00:53:53 +0900

    plolty_chart

--- pages/02_bar_graph.py
+++ pages/02_bar_graph.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import streamlit as st
+import plotly.graph_objects as go
+
 import common
 
 st.title("Bar Graph")
@@ -8,10 +10,29 @@ df = common.get_sales()
 
 genre_counts = df['Genre'].value_counts().sort_values(ascending=False)
 
-plt.bar(genre_counts.index, genre_counts.values)
-plt.xlabel('Genre')
-plt.ylabel('Number of Games')
-plt.title('Number of Video Games by Genre')
-plt.xticks(rotation=90)
+tab1, tab2 = st.tabs(["Pyplot", "Plotly"])
 
-st.pyplot(plt)
+with tab1:
+    plt.bar(genre_counts.index, genre_counts.values)
+    plt.xlabel('Genre')
+    plt.ylabel('Number of Games')
+    plt.title('Number of Video Games by Genre')
+    plt.xticks(rotation=90)
+
+    st.pyplot(plt)
+
+with tab2:
+    fig = go.Figure(data=[go.Bar(x=genre_counts.index, y=genre_counts.values)])
+
+    fig.update_layout(
+        xaxis=dict(
+            title='Genre',
+            tickangle=90,
+        ),
+        yaxis=dict(
+            title='Number of Games',
+        ),
+        title='Number of Video Games by Genre',
+    )
+
+    st.plolty_chart(fig)
