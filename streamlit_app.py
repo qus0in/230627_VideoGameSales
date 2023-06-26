e9b5290254a9033e9521467a576d4eb34c94bc29
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-06-27 00:31:05 +0900

    Video Game Sales

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Video Game Sales")
+st.caption("""
+"Video Game Sales" (비디오 게임 판매 데이터):
+이 데이터셋은 전 세계적으로 발매된 비디오 게임의 판매 정보를 담고 있습니다.
+게임 플랫폼, 장르, 출시 연도 등을 기반으로 다양한 시각화를 통해
+비디오 게임 시장 동향과 인기 게임을 분석할 수 있습니다.
+""")
